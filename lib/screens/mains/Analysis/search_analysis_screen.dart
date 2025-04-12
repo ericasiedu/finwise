5cf7a3b2eb7b0e555d0942d3dc6728451bb6aea4
eric <rocawatts@gmail.com>
2025-04-12 02:43:24 +0000

    FW-8 commit

--- lib/screens/mains/Analysis/search_analysis_screen.dart
+++ lib/screens/mains/Analysis/search_analysis_screen.dart
@@ -1,8 +1,10 @@
 import 'package:finwise/utils/color.dart';
 import 'package:finwise/utils/container_wrapper.dart';
 import 'package:finwise/utils/custom_input.dart';
+import 'package:finwise/utils/primary_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
 
 class SearchAnalysisScreen extends StatefulWidget {
   const SearchAnalysisScreen({super.key});
@@ -22,6 +24,29 @@ class _SearchAnalysisScreenState extends State<SearchAnalysisScreen> {
       ),
     );
 
+    List<String> items = ['option 1', 'option 2', 'option 3'];
+    String? selectedValue;
+
+    String? _selectedOption; // Can be null initially
+
+    final List<String> _options = ['Income', 'Expenses'];
+
+    TextEditingController _dateController = TextEditingController();
+    DateTime? _selectedDate;
+
+    Future<void> _selectDate(BuildContext context) async {
+      final DateTime? pickedDate = await showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime(2000),
+        lastDate: DateTime(2100),
+      );
+
+      if (pickedDate != null) {
+        _dateController.text = DateFormat('dd/MM/yyyy').format(pickedDate);
+      }
+    }
+
     return ContainerWrapper(
       showAppBar: true,
       title: 'Search',
@@ -34,7 +59,98 @@ class _SearchAnalysisScreenState extends State<SearchAnalysisScreen> {
           SizedBox(height: 60),
         ],
       ),
-      bodyChild: Column(children: []),
+      bodyChild: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('Categories', style: TextStyle(fontSize: 18)),
+          SizedBox(height: 5),
+          DropdownButtonFormField<String>(
+            isExpanded: true,
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: lightGreen,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(30),
+                borderSide: BorderSide.none,
+              ),
+              contentPadding: EdgeInsets.symmetric(
+                horizontal: 16,
+                vertical: 12,
+              ),
+            ),
+            value: selectedValue,
+            hint: Text('Select the category'),
+            items:
+                items.map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+            onChanged: (String? value) {
+              setState(() {
+                selectedValue = value;
+              });
+            },
+          ),
+          SizedBox(height: 15),
+          Text('Date', style: TextStyle(fontSize: 18)),
+          SizedBox(height: 5),
+          TextField(
+            controller: _dateController, // <-- Added this
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: lightGreen,
+              hintText: '30/04/2025',
+              hintStyle: TextStyle(color: placeHolderColor),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(50.0),
+                borderSide: BorderSide.none,
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(50.0),
+                borderSide: BorderSide.none,
+              ),
+              contentPadding: EdgeInsets.symmetric(
+                horizontal: 16.0,
+                vertical: 12.0,
+              ),
+              suffixIcon: Icon(Icons.calendar_month, color: primaryColor),
+            ),
+            readOnly: true,
+            onTap: () => _selectDate(context),
+          ),
+          SizedBox(height: 30),
+          Text('Report', style: TextStyle(fontSize: 18)),
+          SizedBox(height: 5),
+          Row(
+            children:
+                _options.map((option) {
+                  return Row(
+                    children: [
+                      Radio<String>(
+                        value: option,
+                        groupValue: _selectedOption,
+                        onChanged: (String? value) {
+                          setState(() {
+                            _selectedOption = value;
+                          });
+                        },
+                        activeColor: primaryColor,
+                      ),
+                      Text(option),
+                      SizedBox(width: 8), // Add spacing between options
+                    ],
+                  );
+                }).toList(),
+          ),
+          SizedBox(height: 30),
+          Center(
+            child: PrimaryButton(
+            title: 'Search', color: primaryColor, onTap: () {}),
+          )
+        ],
+      ),
     );
   }
 }
